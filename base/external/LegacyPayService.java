63b53580d3ffa82394285ab4911e95ee16e056ed
Syed Waleed Hussain <syedw4420@gmail.com>
2026-02-26 16:46:51 +0500

    Restore LegacyPayService class definition

--- base/external/LegacyPayService.java
+++ base/external/LegacyPayService.java
@@ -1,7 +1,9 @@
 package base.external;
 
 public class LegacyPayService {
+    
     public void makePayment(double valueInDollars) {
         System.out.println("[LegacyPayService] Processing payment of $" + valueInDollars);
     }
+
 }
