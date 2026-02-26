514cbfdaf8659a2813adab389d42a3a119fd9253
Syed Waleed Hussain <syedw4420@gmail.com>
2026-02-26 17:45:46 +0500

    Add files via upload

---
+++ exercise4/integration/LegacyPayIntegration.java
@@ -0,0 +1,18 @@
+package base.integration;
+
+import base.core.PaymentService;
+import base.external.LegacyPayService;
+
+public class LegacyPayIntegration implements PaymentService {
+    private LegacyPayService legacyService;
+
+    public LegacyPayIntegration(LegacyPayService legacyService) {
+        this.legacyService = legacyService;
+    }
+
+    @Override
+    public boolean pay(double amount) {
+        legacyService.makePayment(amount);
+        return true; 
+    }
+}
