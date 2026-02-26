bdc8b864757bd07f174fb71a8087fa5788b53f43
Syed Waleed Hussain <syedw4420@gmail.com>
2026-02-26 16:49:09 +0500

    Reformat Main.java for consistency

--- base/client/Main.java
+++ base/client/Main.java
@@ -6,12 +6,10 @@ import base.core.PaymentService;
 
 public class Main {
     public static void main(String[] args) {
-       
         LegacyPayService legacyService = new LegacyPayService();
-        
         PaymentService adapter = new LegacyPayIntegration(legacyService);
-        
         PaymentClient client = new PaymentClient(adapter);
         client.processClientPayment(250.0);
     }
+
 }
