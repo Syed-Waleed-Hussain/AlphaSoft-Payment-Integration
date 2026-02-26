c03bfaa74ef255c037bc0b4a8f1da4bcad1be343
Syed Waleed Hussain <syedw4420@gmail.com>
2026-02-26 17:51:50 +0500

    Restore MockPayService implementation

--- exercise5/integration/MockPayService.java
+++ exercise5/integration/MockPayService.java
@@ -1,11 +1,9 @@
 package exercise5.integration;
-
 import exercise5.core.PaymentService;
 
 public class MockPayService implements PaymentService {
     @Override
     public boolean pay(double amount) {
-        
         System.out.println("[MockPayService] Simulating payment of $" + amount);
         System.out.println("[Test Mode] No real transaction executed.");
         return true;
